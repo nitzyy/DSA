74838a07b4fccc7fa2773a88f35395a0cd79d2d7
Nitya Gupta <gnitya2507@gmail.com>
2026-02-16 22:09:01 +0530

    initial commit

---
+++ Day3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    int comparisons = 0;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    int found = -1;
+
+    for(int i = 0; i < n; i++) {
+        comparisons++;
+        if(arr[i] == k) {
+            found = i;
+            break;
+        }
+    }
+
+    if(found != -1) {
+        printf("Found at index %d\n", found);
+    } else {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d\n", comparisons);
+
+    return 0;
+}
